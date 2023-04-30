3c8c5fd27e9220fbe0b565cda9724332f2b8d72c
boybook <fangyizhou1997@qq.com>
2023-04-30 15:40:35 +0800

    统一修复（重写）一轮水晶战争的bug和新增真实网络延迟的HUD显示

--- src/main/java/com/boybook/BorderAPI/OutDo.java
+++ src/main/java/com/boybook/BorderAPI/OutDo.java
@@ -4,6 +4,8 @@ import cn.nukkit.Player;
 import cn.nukkit.block.Block;
 import cn.nukkit.event.entity.EntityDamageByBlockEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
+import it.unimi.dsi.fastutil.longs.Long2LongMap;
+import it.unimi.dsi.fastutil.longs.Long2LongOpenHashMap;
 
 @FunctionalInterface
 public interface OutDo {
@@ -13,6 +15,7 @@ public interface OutDo {
     class Damage implements OutDo {
         private final float beDamagedOut;
         private final int beDamagedOutTicks;
+        private final Long2LongMap playerLastDamageTime = new Long2LongOpenHashMap();
 
         public Damage() {
             this(1, 10);
@@ -24,9 +27,11 @@ public interface OutDo {
 
         @Override
         public void outDo(Player player, int ticks) {
-            if (this.beDamagedOut > 0 && player.noDamageTicks <= 0) {
+            if (this.beDamagedOut > 0 && System.currentTimeMillis() > playerLastDamageTime.getOrDefault(player.getLoaderId(), 0) + beDamagedOutTicks * 50L) {
+                long nextAllowAttack = player.getNextAllowAttack();  // 让毒圈不造成攻击冷却（保持原来的攻击冷却时间）
                 player.attack(new EntityDamageByBlockEvent(Block.get(Block.AIR), player, EntityDamageEvent.DamageCause.VOID, this.beDamagedOut));
-                player.noDamageTicks = this.beDamagedOutTicks;
+                playerLastDamageTime.put(player.getLoaderId(), System.currentTimeMillis());
+                player.setNextAllowAttack(nextAllowAttack);
             }
         }
     }
